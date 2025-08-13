415ba230bff4b5fd86e92c53dd0adcd674b23bd6
Ashish Brahma <ashish.brahma@outlook.com>
2025-08-13 23:59:24 +0530

    Place hugo files in root and remove version 1 site directory.

--- site/js/main.js
+++
@@ -1,6 +0,0 @@
-// Invoke function to load content.
-contentController.loadContent();
-
-// Initiate scrollspy event listener.
-document.addEventListener(K.domContentLoadedEvent, 
-						sectionController.listenCurrentSection(event));
